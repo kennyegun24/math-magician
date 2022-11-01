deb12d0e5f261c091aaf43fa332ce5a05e931f34
kenny Egun <kennyegun240@gmail.com>
2022-11-01 13:46:21 +0100

    Remove missing import files and fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,4 @@
-import '../index.css';
+import './calculator.css';
 import React from 'react';
 
 class Calculator extends React.Component {
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,4 +9,4 @@ root.render(
   </React.StrictMode>,
 );
 
-reportWebVitals();
+// reportWebVitals();
